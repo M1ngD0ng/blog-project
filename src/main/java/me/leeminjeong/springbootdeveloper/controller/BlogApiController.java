b31ffa8a098cfe860bba5c0f4d90c3da321416e5
alswjd6807@naver.com <alswjd6807@naver.com>
2024-03-28 15:09:19 +0900

    articles list findAll api

--- src/main/java/me/leeminjeong/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/leeminjeong/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,13 @@ package me.leeminjeong.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.leeminjeong.springbootdeveloper.domain.Article;
 import me.leeminjeong.springbootdeveloper.dto.AddArticleRequest;
+import me.leeminjeong.springbootdeveloper.dto.ArticleResponse;
 import me.leeminjeong.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -24,5 +25,17 @@ public class BlogApiController {
         // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED) // 201 == created
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+        // findAll 메서드를 호출한 다음 응답용 객체인 ArticleResponse로 파싱해 body에 담아 클라이언트에게 전송
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
--- src/main/java/me/leeminjeong/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/leeminjeong/springbootdeveloper/service/BlogService.java
@@ -6,6 +6,8 @@ import me.leeminjeong.springbootdeveloper.dto.AddArticleRequest;
 import me.leeminjeong.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor // 빈을 생성자로 생성, final이 붙거나 @NotNull이 붙은 필드로 생성자 추가
 @Service // 해당 클래스를 빈으로 등록
 public class BlogService {
@@ -15,5 +17,9 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request){ // AddArticleRequest 클래스에 저장된 값들을 article 데이터베이스에 저장ㅇ
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll(){
+        return blogRepository.findAll();
     }
 }
