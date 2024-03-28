33df277ece6773605efcf9c425c9aede0e7c1772
alswjd6807@naver.com <alswjd6807@naver.com>
2024-03-28 14:40:57 +0900

    article add api

---
+++ src/test/java/me/leeminjeong/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -0,0 +1,77 @@
+package me.leeminjeong.springbootdeveloper.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.leeminjeong.springbootdeveloper.domain.Article;
+import me.leeminjeong.springbootdeveloper.dto.AddArticleRequest;
+import me.leeminjeong.springbootdeveloper.repository.BlogRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest // 테스트용 애플리케이션 컨텍스트
+@AutoConfigureMockMvc // MockMvc 생성 및 자동 구성
+class BlogApiControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper; // 직렬화, 역직렬화를 위한 클래스
+    // 자바 객체를 JSON 데이터로 변환 == 직렬화 , 반대로 JSON 데이터를 자바에서 사용하기 위해 자바 객체로 변환 == 역직렬화
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    BlogRepository blogRepository;
+
+    @BeforeEach
+    public void mockMvcSetUp(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+        blogRepository.deleteAll();
+    }
+
+    @DisplayName("addArticle : 블로그 글 추가에 성공한다.")
+    @Test
+    public void addArticle() throws Exception{
+        // given 글 추가에 필요한 요청 객체를 만듦
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+        final AddArticleRequest userRequest = new AddArticleRequest(title, content);
+
+        // 객체 JSON으로 직렬화
+        final String requestBody = objectMapper.writeValueAsString(userRequest);
+
+        // when 블로그 글 추가 API에 요청을 보냄, 요청 타입은 JSON이며 given절에서 미리 만들어둔 객체를 요청 본문으로 함께 보냄
+        // 설정한 내용을 바탕으로 요청 전송
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+
+        // then 응답 코드가 201 created인지 확인, Blog를 전체 조회해 크기가 1인지 확인하고, 실제로 저장된 데이터와 요청 값을 비교
+        result.andExpect(status().isCreated());
+
+        List<Article> articles = blogRepository.findAll();
+
+        assertThat(articles.size()).isEqualTo(1); // 크기가 1인지 검증
+        assertThat(articles.get(0).getTitle()).isEqualTo(title);
+        assertThat(articles.get(0).getContent()).isEqualTo(content);
+    }
+}
