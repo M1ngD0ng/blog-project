202702771640a1421efb414cd0975696eafcf8e8
alswjd6807@naver.com <alswjd6807@naver.com>
2024-03-30 00:39:03 +0900

    view with thymeleaf

---
+++ src/main/java/me/leeminjeong/springbootdeveloper/controller/ExampleController.java
@@ -0,0 +1,37 @@
+package me.leeminjeong.springbootdeveloper.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+public class ExampleController {
+    // Model 객체는 HTML 쪽으로 값을 넘겨주는 객체로, 따로 생성할 필요없이 인자로 선언해주기만 하면 됨
+    @GetMapping("/thymeleaf/example")
+    public String thymeleafExample(Model model) {
+        Person examplePerson = new Person();
+        examplePerson.setId(1L);
+        examplePerson.setName("홍길동");
+        examplePerson.setAge(11);
+        examplePerson.setHobbies(List.of("운동","독서"));
+
+        model.addAttribute("person", examplePerson);
+        model.addAttribute("today", LocalDate.now());
+
+        return "example";
+    }
+
+    @Setter
+    @Getter
+    class Person{
+        private Long id;
+        private String name;
+        private int age;
+        private List<String> hobbies;
+    }
+}
--- src/main/java/me/leeminjeong/springbootdeveloper/domain/Article.java
+++ src/main/java/me/leeminjeong/springbootdeveloper/domain/Article.java
@@ -5,6 +5,10 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -32,6 +36,15 @@ public class Article {
         this.title = title;
         this.content = content;
     }
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
     // @NoArgsConstructor(access = AccessLevel.PROTECTED)로 대체 가능한 코드
 //    protected Article(){ // 기본 생성자
 //    }
